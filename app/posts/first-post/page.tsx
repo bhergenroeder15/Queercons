1b3f32c249cfef0da89646c71e96ffd775720a6e
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-06-08 19:25:13 -0400

    Module css add

--- app/posts/first-post/page.tsx
+++ app/posts/first-post/page.tsx
@@ -1,16 +1,22 @@
+'use client'
 import Link from 'next/link';
-
-export const metadata = {
-  title: 'First Post',
-}
+import Script from 'next/script';
+import Layout from '../../../components/layout'
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }}
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
